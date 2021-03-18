ddf2b30a037d01f0ede3431b119850a5511a5d42
xX-Rafa-Xx <56598807+xX-Rafa-Xx@users.noreply.github.com>
2021-03-17 18:43:58 -0600

    primer commit-pruebas

---
+++ src/components/Test.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>   
+    <!--Funciona cuando el usuario da un click    
+    <button type="button" v-on:click="saludar(message)">
+        SAY HI ME
+    </button>
+    <input v-model = "message" placeholder="Escribe tu nombre mi simio">
+    
+    <hr>
+    -->
+
+    <!--Funciona cuando el usuario está tipeando
+    <input type="text" v-on:keyup.enter="typing">
+  -->
+<div>
+  {{msg}}
+</div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hola Vuejs",
+      user: {
+        firstName: "Fernando",
+        lastName: "Jose",
+      },
+      myhtml: "<h2>Desde el html</h2>",
+      showName: false,
+      tasks: [
+        { title: "Hoy no me quiero levantar" },
+        { title: "hoy no cenaré" },
+        { title: "hoy no dormiré" },
+        { title: "hoy no me bañaré" }
+      ]
+    }
+  },
+  methods: {
+   // saludar(message){
+     //   alert("Hola "+message+" Mucho gusto");
+    //},
+    typing(e){
+      console.log("Dio un enter");
+    }
+    },
+     computed: {
+      fullName(){
+        return this.user.firstName + " " + this.user.lastName;
+      }
+    }, //para aceptar datos desde afuera
+    props: {
+      msg:{
+        type: String 
+      }
+    }    
+}
+</script>
+
+<style media="screen">
+</style>
